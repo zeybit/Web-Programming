eaad938dbecd7c2bfa3f5430613612de4ff6bf69
zeynep zıvalı <148189146+zeybit@users.noreply.github.com>
2024-04-18 11:03:31 +0300

    Add files via upload

---
+++ New folder (2)/script.js
@@ -0,0 +1,13 @@
+function fnTopla(a,b){
+document.write(a+b);}
+
+let  carpim =(a,b)=>{
+document.getElementById("etiket1")
+.innerHTML=a*b;
+
+}
+let cikar=()=>{
+let s1=document.getElementById("sayi1").value;
+let s2=document.getElementById("sayi2").value;
+document.getElementById("sonuc"),innerHTML=s1-s2;
+}
